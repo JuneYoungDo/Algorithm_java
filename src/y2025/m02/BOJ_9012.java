32223fa2313a24701a701eb67ce807698fa67b13
JuneYoungDo <ehwnsdud1004@naver.com>
2025-02-27 14:12:16 +0900

    [2025/02/27] BOJ_9012

--- src/y2025/m02/BOJ_9012.java
+++ src/y2025/m02/BOJ_9012.java
@@ -1,4 +1,35 @@
 package y2025.m02;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BOJ_9012 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long testcase = Long.parseLong(sc.nextLine());
+        for (int i = 0; i < testcase; i++) {
+            boolean flag = true;
+            Stack<Character> s = new Stack<>();
+            String input = sc.nextLine();
+
+            for (int j = 0; j < input.length(); j++) {
+                if (input.charAt(j) == '(') {
+                    s.push(input.charAt(j));
+                } else {
+                    if (s.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    s.pop();
+                }
+            }
+            if (!flag || !s.isEmpty()) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+
+    }
 }
